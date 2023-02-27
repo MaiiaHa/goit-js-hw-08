5e5730716a644388ceea4c1016952bf1f55d2faf
MaiiaHa <maiia.robeiko@ukr.net>
2023-02-27 02:45:31 +0100

    correction

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,8 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -9,73 +14,37 @@ const galleryMarkup = createGalleryCardsMarkup(galleryItems);
 
 imgsContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-imgsContainer.addEventListener('click', onGalleryContainerClick);
-
 function createGalleryCardsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-      <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-          />
-        </a>
-     </div>`;
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
     })
     .join('');
 }
 
-function onGalleryContainerClick(evt) {
-  evt.preventDefault(); // do not save img
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: '250ms',
+// });
 
-  const isGalleryContainImg = evt.target.classList.contains('gallery__image');
-  if (!isGalleryContainImg) {
-    return;
-  }
-  evt.target.src = evt.target.dataset.source; // chance for a big picture
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', {
+  // do something…
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
 
-  const instance = basicLightbox.create(
-    `
-  <img src="${evt.target.src}" width="800" height="600">
-  `
-  );
-  instance.show();
-
-  imgsContainer.addEventListener(
-    'keydown',
-    evt => {
-      if (evt.code === 'Escape') {
-        instance.close();
-      }
-    },
-    { once: true } // close after first click on escape
-  );
-}
+// gallery.on('error.simplelightbox', function (e) {
+//   console.log(e); // some usefull information
+// });
 
 // add loading = 'lazy';
 
-// intersection observer - 'lazy' load
 const lazyImgs = document.querySelectorAll('.gallery__image');
 lazyImgs.forEach(img => {
   img.loading = 'lazy';
   img.height = '240px'; // should be for lazy works
-});
-
-console.log(lazyImgs);
-
-function handleIntersection(entries) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting);
-    entry.target.src = entry.target.dataset.source;
-    observer.unobserve(entry.target);
-  });
-}
-
-const observer = new IntersectionObserver(handleIntersection);
-lazyImgs.forEach(image => {
-  observer.observe(image);
 });
