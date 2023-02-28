744e81cf1c35c7c78354edf46ad2cca812520318
MaiiaHa <maiia.robeiko@ukr.net>
2023-02-28 21:25:45 +0100

    updates

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Vimeo from '@vimeo/player';
+import lodash from 'lodash.throttle';
+/* HTML містить <iframe> з відео для Vimeo плеєра. 
+Напиши скрипт, який буде зберігати поточний час відтворення 
+відео у локальне сховище і, після перезавантаження сторінки, 
+продовжувати відтворювати відео з цього часу. 
+*/
+const iframe = document.querySelector('iframe'); // вибрали тег з відео
+const player = new Vimeo.Player(iframe); // передали тег відео в вімео плеєр створили плеєр
+// плеєр увімкнутий
+player.on('play', function () {
+  console.log('played the video!');
+});
 
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+// on(event: string, callback: function): void
+// Add an event listener for the specified event. Will call
+// the callback with a single parameter, data, that contains
+// the data for that event.See events below for details.
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
