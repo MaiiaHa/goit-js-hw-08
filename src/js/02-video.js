370cd7ced8bc0bc3a1696f2c2aae6b9227eb5b04
MaiiaHa <maiia.robeiko@ukr.net>
2023-03-04 22:06:55 +0100

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,44 @@
 import Vimeo from '@vimeo/player';
-import lodash from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 /* HTML містить <iframe> з відео для Vimeo плеєра. 
 Напиши скрипт, який буде зберігати поточний час відтворення 
 відео у локальне сховище і, після перезавантаження сторінки, 
 продовжувати відтворювати відео з цього часу. 
 */
+
+// ініціалізували плеєр https://github.com/vimeo/player.js/#pre-existing-player
+
 const iframe = document.querySelector('iframe'); // вибрали тег з відео
-const player = new Vimeo.Player(iframe); // передали тег відео в вімео плеєр створили плеєр
-// плеєр увімкнутий
-player.on('play', function () {
-  console.log('played the video!');
-});
+const player = new Vimeo(iframe); // Player прибрали, передали тег відео в вімео плеєр, створили плеєр
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// відстежуємо подію оновлення часу відтворення timeupdate, throttle передає час в localStorage з інтервалом через 1сек.
 
-// on(event: string, callback: function): void
-// Add an event listener for the specified event. Will call
-// the callback with a single parameter, data, that contains
-// the data for that event.See events below for details.
+player.on('timeupdate', throttle(updatedTimeVideo, 1000));
 
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-};
+function updatedTimeVideo(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
 
-player.on('play', onPlay);
+let currentTime = localStorage.getItem('videoplayer-current-time');
+
+//https://github.com/vimeo/player.js/#setcurrenttimeseconds-number-promisenumber-rangeerrorerror
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+    seconds = currentTime;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        currentTime = 0;
+        break;
+
+      default:
+        // some other error occurred
+        currentTime = 0;
+        break;
+    }
+  });
